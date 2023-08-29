147b300aced8d61a0f02fcd17f42f3fe9a2773ee
Olga <olga-kh.neru@yandex.ru>
2023-08-29 22:14:02 +0900

    сдать дз массивы 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,78 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
+    private static int[] weight = new int[]{1, 2, 3};
+    private static double[] drobi = new double[]{1.57, 7.654, 9.986};
+    private static int[] massa = {80, 78, 77, 75, 74, 73, 73, 72, 0, 0, 0, 0};
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task2();
+        task3();
+        task4();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 0; i < weight.length; i++) {
+            if (i != weight.length - 1) {
+                System.out.print(weight[i] + ", ");
+            } else {
+                System.out.print(weight[i]);
+            }
         }
+        System.out.println();
+        for (int i = 0; i < drobi.length; i++) {
+            if (i != drobi.length - 1) {
+                System.out.print(drobi[i] + ", ");
+            } else {
+                System.out.print(drobi[i]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < massa.length; i++) {
+            if (i != massa.length - 1) {
+                System.out.print(massa[i] + ", ");
+            } else {
+                System.out.print(massa[i]);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = weight.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(weight[i] + ", ");
+            } else {
+                System.out.print(weight[i]);
+            }
+        }
+        System.out.println();
+        for (int i = drobi.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(drobi[i] + ", ");
+            } else {
+                System.out.print(drobi[i]);
+            }
+        }
+        System.out.println();
+        for (int i = massa.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(massa[i] + ", ");
+            } else {
+                System.out.print(massa[i]);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 0; i < weight.length; i++) {
+            if (weight[i] % 2 != 0) weight[i]++;
+        }
+        System.out.println(Arrays.toString(weight));
     }
 }
